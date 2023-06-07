ea500bef1b0a4a523600562732f1a12f52432cd3
GabaeOh <rkqo4577@gmail.com>
2023-06-07 15:23:03 +0900

    정리

--- Members/Members_DML.sql
+++ Members/Members_DML.sql
@@ -1,81 +1,75 @@
--- GENDER CODE
+-- INSERT INTO table_name
+-- VALUES (value1, value2, value3, ...);
+-- INSERT INTO table_name (column1, column2, column3, ...)
+-- VALUES (value1, value2, value3, ...);
+
+
+-- gender_code  성별 코드 생성
 INSERT INTO gender_code
 (GENDER, GENDER_CODE_ID)
 VALUES
-('남성', 'G-01');
+('남', 'G-01');
 
 INSERT INTO gender_code
 (GENDER, GENDER_CODE_ID)
 VALUES
-('여성', 'G-03');
+('여', 'G-04');
 
-INSERT INTO gender_code
-(GENDER, GENDER_CODE_ID)
-VALUES
-('양성', 'G-04');
-
--- ADDRESS CODE
+-- address_code 주소 코드 생성
 INSERT INTO address_code
-(ADDRESS, ADDRESS_CODE_ID)
+(ADDRESS,ADDRESS_CODE_ID)
 VALUES
-('서울시 강남구', 'A-01'),
-('경기도 성남시', 'A-02'),
-('인천시 중구', 'A-03'),
-('대전시 유성구', 'A-04'),
-('부산시 해운대구', 'A-05');
+('서울시', 'A-01'),
+('부산시', 'A-02'),
+('경기도', 'A-03'),
+('대전시', 'A-04'),
+('인천시', 'A-05');
 
--- MEMBERS CODE
+-- members 회원정보 입력
 INSERT INTO members
-(NAME, PHONE_NUMBERS, AGE, MEMBERS_ID, GENDER_CODE_ID, ADDRESS_CODE_ID)
+(AGE,NAME,TELEPHONE,MEMBERS_ID,GENDER_CODE_ID,ADDRESS_CODE_ID)
 VALUES
-('John Doe', '010-1234-5678', 30, 'M-01', 'G-01','A-01'),
-('Emily Smith', '010-9876-5432', 25, 'M-02', 'G-03','A-02'),
-('David Lee', '010-5678-1234', 35, 'M-03', 'G-01','A-03'),
-('Sarah Kim', '010-4321-8765', 28, 'M-04', 'G-03','A-04'),
-('홍길동', '010-1111-2222', 33, 'M-05', 'G-01','A-05');
+(30, '홍길동', '010-1234-5678', 'M-01', 'G-01', 'A-01'),
+(25, '박영희', '010-9876-5432', 'M-02', 'G-04', 'A-02'),
+(35, '김철수', '010-1111-2222', 'M-03', 'G-01', 'A-03'),
+(28, '이영미', '010-3333-4444', 'M-04', 'G-04', 'A-04'),
+(32, '정민호', '010-5555-6666', 'M-05', 'G-01', 'A-05');
 
--- MEMBERS_login
-INSERT INTO members_login
-(E_MAIL, MEMBERS_ID, MEMBERS_LOGIN_ID)
-VALUES
-('johndoe@example.com', 'M-01', 'L-01'),
-('emilysmith@example.com', 'M-02', 'L-02'),
-('davidlee@example.com', 'M-03', 'L-03'),
-('sarahkim@example.com', 'M-04', 'L-04'),
-('honggildong@example.com', 'M-05', 'L-05');
-
--- HOBBY CODE
+-- members_hobby 취미 코드 생성
 INSERT INTO hobby_code
 (HOBBY, HOBBY_CODE_ID)
 VALUES
-('음악감상', 'H-01'),
-('독서', 'H-02'),
-('요가', 'H-03'),
-('여행', 'H-04'),
-('게임', 'H-05'),
-('요리', 'H-06'),
-('등산', 'H-07'),
-('테니스', 'H-08');
+('요리', 'H-01'),
+('등산', 'H-02'),
+('독서', 'H-03'),
+('영화감상', 'H-04'),
+('음악 감상', 'H-05'),
+('요가', 'H-06'),
+('여행', 'H-07'),
+('사진촬영', 'H-08'),
+('수영', 'H-09');
 
--- MEMBERS_HOBBY
+-- hobby_code 취미코드와 회원코드 연결
 INSERT INTO members_hobby
-(HOBBY_CODE_ID, MEMBERS_ID)
+(MEMBERS_ID, HOBBY_CODE_ID)
 VALUES
-('H-01', 'M-01'),
-('H-02', 'M-01'),
-('H-03', 'M-02'),
-('H-04', 'M-02'),
-('H-05', 'M-03'),
-('H-01', 'M-03'),
-('H-06', 'M-04'),
-('H-07', 'M-04'),
-('H-04', 'M-05'),
-('H-08', 'M-05');
+('M-01', 'H-01'),
+('M-01', 'H-02'),
+('M-02', 'H-03'),
+('M-02', 'H-04'),
+('M-03', 'H-05'),
+('M-03', 'H-06'),
+('M-04', 'H-07'),
+('M-04', 'H-08'),
+('M-05', 'H-02'),
+('M-05', 'H-09');
 
-
-
-
-
-
-
-
+-- membewrs_login 로그인 코드
+INSERT INTO members_login
+(EMAIL,MEMBERS_ID,MEMBERS_LOGIN_ID)
+VALUES
+('hong@gmail.com', 'M-01', 'L-01'),
+('park.younghee@naver.com', 'M-02', 'L-02'),
+('kimchulsu@gmail.com', 'M-03', 'L-03'),
+('leeyoungmi@naver.com', 'M-04', 'L-04'),
+('jmh123@gmail.com', 'M-05', 'L-05');address_code
--- Members/Members_Deletes.sql
+++ Members/Members_Deletes.sql
@@ -1,26 +1,9 @@
--- SELECT column1, column2, ...  // 기본 명령어
--- FROM table_name
--- WHERE condition
--- ORDER BY column1, column2, ... ASC|DESC;
-
--- DELETE FROM table_name WHERE condition; //삭제하는 법 
 
 -- members테이블 속 data 삭제하기 / pk를 활용하여 삭제 하기, 연결된 pk를 먼저 삭제해줘야 됨(순서 중요)
 
-DELETE FROM hobby_code -- hobby_code 테이블 삭제
-WHERE MEMBERS_ID LIKE '%'||'H'||'%';
-
-DELETE FROM members_hobby -- members_code 테이블 삭제
-WHERE MEMBERS_ID LIKE '%'||'M'||'%';
-
-DELETE FROM membewrs_login  -- membewrs_login 테이블 삭제
-WHERE MEMBERS_ID LIKE '%'||'M'||'%';
-
-DELETE FROM address_code -- address_code_code 테이블 삭제
-WHERE ADDRESS_CODE_ID LIKE '%'||'A'||'%';
-
-DELETE FROM gender_code -- gender_code_code 테이블 삭제
-WHERE GENDER_CODE_ID LIKE '%'||'G'||'%';
-
-DELETE FROM members -- members 테이블 삭제
-WHERE MEMBERS_ID LIKE '%'||'M'||'%';
+DELETE FROM members_hobby ;
+DELETE FROM membewrs_login ;
+DELETE FROM hobby_code ;
+DELETE FROM members ;
+DELETE FROM address_code ;
+DELETE FROM gender_code ;
