fa29829ae985fbd1164d3597647976218779d60d
GabaeOh <rkqo4577@gmail.com>
2023-06-07 15:48:50 +0900

    수정

--- selectwhereRangeAndEquals.sql
+++ selectwhereRangeAndEquals.sql
@@ -2,6 +2,6 @@
 
 -- 조건 : CustomerID가 50에서 89이고, City가 Berlin인 고객
 SELECT * FROM Customers
-WHERE (CustomerID  BETWEEN 50 AND 89) AND (City = 'London');  
+WHERE CustomerID  BETWEEN 50 AND 89 AND City IN ('London');  
 
 -- Number of Records: 2
