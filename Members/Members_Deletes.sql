143ba2d9532a043eba41487659227b62b5db4890
GabaeOh <rkqo4577@gmail.com>
2023-06-07 12:25:52 +0900

    수정

--- Members/Members_Deletes.sql
+++ Members/Members_Deletes.sql
@@ -5,21 +5,22 @@
 
 -- DELETE FROM table_name WHERE condition; //삭제하는 법 
 
+-- members테이블 속 data 삭제하기 / pk를 활용하여 삭제 하기, 연결된 pk를 먼저 삭제해줘야 됨(순서 중요)
 
-DELETE FROM hobby_code
+DELETE FROM hobby_code -- hobby_code 테이블 삭세 
 WHERE MEMBERS_ID LIKE '%'||'H'||'%';
 
-DELETE FROM members_hobby
+DELETE FROM members_hobby -- members_code 테이블 삭세
 WHERE MEMBERS_ID LIKE '%'||'M'||'%';
 
-DELETE FROM membewrs_login
+DELETE FROM membewrs_login  -- membewrs_login 테이블 삭세
 WHERE MEMBERS_ID LIKE '%'||'M'||'%';
 
-DELETE FROM address_code
+DELETE FROM address_code -- address_code_code 테이블 삭세
 WHERE ADDRESS_CODE_ID LIKE '%'||'A'||'%';
 
-DELETE FROM gender_code
+DELETE FROM gender_code -- gender_code_code 테이블 삭세
 WHERE GENDER_CODE_ID LIKE '%'||'G'||'%';
 
-DELETE FROM members
+DELETE FROM members -- members 테이블 삭세
 WHERE MEMBERS_ID LIKE '%'||'M'||'%';
